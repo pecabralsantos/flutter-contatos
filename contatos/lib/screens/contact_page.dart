0304efda4c845f042ffc3da295a2d0431b52b124
Pedro Cabral <pecabralsantos@gmail.com>
2021-06-22 13:10:43 -0300

    Tela de detalhe do contato.

--- contatos/lib/screens/contact_page.dart
+++ contatos/lib/screens/contact_page.dart
@@ -1,8 +1,6 @@
-import 'package:contatos/controllers/feed_controller.dart';
-import 'package:contatos/service/model/contact_dto.dart';
 import 'package:contatos/utils/route_arguments.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
+import 'package:flutter/painting.dart';
 
 class ContactPage extends StatefulWidget {
   const ContactPage({Key? key}) : super(key: key);
@@ -12,16 +10,78 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
-  FeedController _controller = GetIt.I.get<FeedController>();
-
   @override
   Widget build(BuildContext context) {
     final args =
         ModalRoute.of(context)!.settings.arguments as ContactRouteArguments;
-    _controller.getContact(args.contactDTO);
     return Scaffold(
-      body: Center(
-        child: Text(args.contactDTO.name),
+      appBar: AppBar(elevation: 0),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.maxFinite,
+            height: 250,
+            color: Colors.grey,
+            child: Image.network(args.contactDTO.photo, fit: BoxFit.cover),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Text(args.contactDTO.name, style: TextStyle(fontSize: 24)),
+          ),
+          Divider(thickness: 2),
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+            child: Row(
+              children: [
+                Icon(Icons.phone_outlined),
+                SizedBox(width: 16),
+                Text(args.contactDTO.phone),
+              ],
+            ),
+          ),
+          Divider(thickness: 2, indent: 56),
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+            child: Row(
+              children: [
+                Icon(Icons.email_outlined),
+                SizedBox(width: 16),
+                Text(args.contactDTO.email),
+              ],
+            ),
+          ),
+          Divider(thickness: 2, indent: 56),
+          Container(
+            padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+            child: Row(
+              children: [
+                Icon(Icons.date_range_outlined),
+                SizedBox(width: 16),
+                Text(args.contactDTO.birthday),
+              ],
+            ),
+          ),
+          Divider(thickness: 2),
+          Container(
+            margin: EdgeInsets.fromLTRB(16, 16, 0, 8),
+            child: Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: [
+                Icon(Icons.person_outlined),
+                Container(
+                  width: 250,
+                  margin: EdgeInsets.fromLTRB(16, 0, 8, 0),
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: Text(args.contactDTO.about),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
